1e2aaf31c05458c05f66f0b33c674e9e755be0e9
eplamondon2 <eplamondon@hyundaistraymond.ca>
2026-05-22 16:15:59 -0400

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "📦 Installation de Chromium..."
-playwright install chromium
-playwright install-deps chromium
-echo "✅ Chromium installé"
 echo "🚀 Démarrage de l'application..."
-python app.py &
-python scheduler.py
+python app.py
